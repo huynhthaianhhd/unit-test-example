7e9f946ba03ef1dbc133c9c9505392664988886a
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-12-29 03:29:49 +0700

    test travis

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Point = require("./models/Point");
 const p1 = new Point(1, 12312);
 const p2 = new Point(1.23, 12); 
 const p3 = new Point(123123, 1);
-//abc
+//abcdddd
 const TriangleTest = new Triangle(p1, p2, p3);
 console.log("Cal: ", p1.calDistance(p2));
 console.log("Phân Loại: ", TriangleTest.classifyTriangle());
