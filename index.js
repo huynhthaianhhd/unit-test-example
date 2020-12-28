a13a8aa1a9364030af09ba84ec495539c3b8395a
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-12-28 23:56:04 +0700

    test build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Point = require("./models/Point");
 const p1 = new Point(0, 0);
 const p2 = new Point(0, 1);
 const p3 = new Point(1, 0);
-//a
+//abc
 const TriangleTest = new Triangle(p1, p2, p3);
 console.log("Phân Loại: ", TriangleTest.classifyTriangle());
 console.log("Chu vi", TriangleTest.perimeter());
